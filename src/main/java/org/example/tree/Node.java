059dccc2bbbdf62376fc243e7687d42a70549fd4
LazariaInk <lorienlored@gmail.com>
2024-08-27 22:20:47 +0300

    finish binary tranversing

--- src/main/java/org/example/tree/Node.java
+++ src/main/java/org/example/tree/Node.java
@@ -1,2 +1,19 @@
-package org.example.tree;public class Node {
+package org.example.tree;
+
+import java.util.List;
+
+public class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
 }
